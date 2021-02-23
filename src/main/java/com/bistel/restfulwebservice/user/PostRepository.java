c01e7a347803d234252891f10673ddd55e996a44
digilog <digilog1q84@gmail.com>
2021-02-23 15:37:31 +0900

    session 1 : HelloWorldController

--- src/main/java/com/bistel/restfulwebservice/user/PostRepository.java
+++
@@ -1,6 +0,0 @@
-package com.bistel.restfulwebservice.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Integer> {
-}
