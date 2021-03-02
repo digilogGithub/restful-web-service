335ee32fd7dc75a906696546750379f554fa3238
digilog <digilog1q84@gmail.com>
2021-03-02 15:09:35 +0900

    session 3 : USER Validation API

--- src/main/java/com/bistel/restfulwebservice/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/bistel/restfulwebservice/exception/CustomizedResponseEntityExceptionHandler.java
@@ -2,8 +2,10 @@ package com.bistel.restfulwebservice.exception;
 
 
 import com.bistel.restfulwebservice.user.UserNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +17,7 @@ import java.util.Date;
 @RestController
 @ControllerAdvice
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
 
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<Object> handlerAllExceptions(Exception ex, WebRequest request) {
@@ -31,6 +34,17 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
                 new Date(), ex.getMessage(), request.getDescription(false)
         );
 
-        return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+        return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        CustomizedExceptionResponse customizedExceptionResponse = new CustomizedExceptionResponse(
+                new Date(), "Validation Failed", ex.getBindingResult().toString()
+        );
+
+        return new ResponseEntity(customizedExceptionResponse, HttpStatus.BAD_REQUEST);
     }
 }
