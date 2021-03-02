d16936e5708cfc18a6d3844d7dabc00ec63e47ab
digilog <digilog1q84@gmail.com>
2021-03-02 11:51:27 +0900

    session 2 : HTTP Status Code

--- src/main/java/com/bistel/restfulwebservice/user/UserController.java
+++ src/main/java/com/bistel/restfulwebservice/user/UserController.java
@@ -1,7 +1,10 @@
 package com.bistel.restfulwebservice.user;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -24,7 +27,13 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
+    public ResponseEntity<User> createUser(@RequestBody User user) {
         User savedUser = userDaoService.saveUser(user);
+
+        URI locationUri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(locationUri).build();
     }
 }
