33e965ef8e52ed7ddc2a864eef4f8233a1ddfcd3
digilog <digilog1q84@gmail.com>
2021-03-02 14:29:58 +0900

    session 2 : Spring AOP Exception Handling

--- src/main/java/com/bistel/restfulwebservice/user/UserNotFoundException.java
+++ src/main/java/com/bistel/restfulwebservice/user/UserNotFoundException.java
@@ -8,7 +8,7 @@ package com.bistel.restfulwebservice.user;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
+//@ResponseStatus(HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(String message) {
         super(message);
