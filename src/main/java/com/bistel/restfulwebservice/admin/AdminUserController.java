bc0af3ec940d41dfdc3a696f31b8e91141163b4a
digilog <digilog1q84@gmail.com>
2021-03-04 11:45:00 +0900

    session 3 : PARAMS REST API Version

--- src/main/java/com/bistel/restfulwebservice/admin/AdminUserController.java
+++ src/main/java/com/bistel/restfulwebservice/admin/AdminUserController.java
@@ -41,7 +41,8 @@ public class AdminUserController {
     }
 
     // GET /admin/users/1 -> /admin/v1/users/1
-    @GetMapping("/v1/users/{id}")
+//    @GetMapping("/v1/users/{id}")
+    @GetMapping(value = "/users/{id}", params = "version=1")
     public MappingJacksonValue retrieveUserV1(@PathVariable int id) {
         User user = userDaoService.findOne(id);
         if (user == null) {
@@ -57,7 +58,8 @@ public class AdminUserController {
         return mappingJacksonValue;
     }
 
-    @GetMapping("/v2/users/{id}")
+    //    @GetMapping("/v2/users/{id}")
+    @GetMapping(value = "/users/{id}", params = "version=2")
     public MappingJacksonValue retrieveUserV2(@PathVariable int id) {
         User user = userDaoService.findOne(id);
         if (user == null) {
