73431b19ccbd962164ed3a9d537c84c0093e1298
digilog <digilog1q84@gmail.com>
2021-03-06 13:10:53 +0900

    session 5 : Create Post Entity

---
+++ src/main/java/com/bistel/restfulwebservice/user/Post.java
@@ -0,0 +1,25 @@
+package com.bistel.restfulwebservice.user;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String description;
+
+    // User : Post -> 1 : (0 ~ N), Main : sub -> Parent : Child
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnore
+    private User user;
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
 insert into user values(10001, sysdate(), 'User1', 'test1111', '701010-1111111');
 insert into user values(10002, sysdate(), 'User2', 'test2222', '801010-2222222');
 insert into user values(10003, sysdate(), 'User3', 'test3333', '901010-1111111');
+
+insert into post values(11001, 'My first post',  10001);
+insert into post values(11002, 'My second post', 10001);
